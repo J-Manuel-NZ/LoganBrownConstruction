f1462f530f69a8df0a1ce8f42aa5333a294b03a3
Justin Manuel <112037970+J-Manuel-NZ@users.noreply.github.com>
2023-06-20 10:19:12 +1200

    Finished Navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,8 @@
+import '../pages/index.css'
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark, faBars} from '@fortawesome/free-solid-svg-icons'
+import logo from '../../public/assets/lbc_logo.jpg'
 
 const Navbar = () => {
 
@@ -10,25 +12,32 @@ const Navbar = () => {
     // need to add a menu for the desktop view here **
     // -------------------------------------
     // **
-      <div className="">
+      <div className="navbar">
+        <img src={logo} alt="Logan Brown Construction Logo" />
           {isMenuOpen ? (
               <FontAwesomeIcon
                   icon={faXmark}
                   alt="Close Icon"
                   onClick={() => setIsMenuOpen(false)}
+                  className='navbar-icon'
               />
           ) : (
               <FontAwesomeIcon
                   icon={faBars}
                   alt="Menu Icon"
                   onClick={() => setIsMenuOpen(true)}
+                  className='navbar-icon'
               />
           )}
           {isMenuOpen && (
-              <ul className="">
-                  <li>one</li>
-                  <li>two</li>
-                  <li>three</li>
+              <ul className="navbar-menu" onClick={() => setIsMenuOpen(false)}>
+                  <li className='navbar-menu_item'>Home</li>
+                  <li className='navbar-menu_item'>Design & Build</li>
+                  <li className='navbar-menu_item'>Renovations</li>
+                  <li className='navbar-menu_item'>Landscaping</li>
+                  <li className='navbar-menu_item'>Recent Work</li>
+                  <li className='navbar-menu_item'>About</li>
+                  <li className='navbar-menu_item'>Contact</li>
               </ul>
           )}
       </div>
